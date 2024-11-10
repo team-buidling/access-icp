34c85b0c5c8205d970af9837a471ade442d8c1a7
Eliezer Rabadon <seiferxiii@gmail.com>
2024-11-10 07:40:11 +0000

    updated canister id, Peso smart contract

--- src/frontend/src/declarations/backend/index.js
+++ src/frontend/src/declarations/backend/index.js
@@ -9,7 +9,7 @@ export { idlFactory } from "./backend.did.js";
  * process.env.CANISTER_ID_<CANISTER_NAME_UPPERCASE>
  * beginning in dfx 0.15.0
  */
-export const canisterId = "3f6pv-baaaa-aaaab-qacoq-cai";
+export const canisterId = "v5htc-aqaaa-aaaak-qtp4a-cai";
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
